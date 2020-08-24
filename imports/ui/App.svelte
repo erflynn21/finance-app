b6f694911494d4acb602f10fe8e9fefdd2e19dbb
Evan Flynn <erflynn21@gmail.com>
2020-08-24 11:49:54 +0800

    got svelte set up

---
+++ imports/ui/App.svelte
@@ -0,0 +1,27 @@
+<script>
+    import Expense from './Expense.svelte';
+    
+    function getExpenses() {
+        return [
+          { _id: 1, text: "This is expense 1" },
+          { _id: 2, text: "This is expense 2" },
+          { _id: 3, text: "This is expense 3" }
+        ];
+      }
+   
+  </script>
+   
+   
+  <div class="container">
+    <header>
+      <h1>Expenses</h1>
+    </header>
+    <ul>
+    {#each getExpenses() as expense}
+      <Expense
+        key={expense._id}
+        expense={expense}
+      />
+    {/each}
+    </ul>
+  </div>
---
+++ imports/ui/Expense.svelte
@@ -0,0 +1,6 @@
+<script>
+    export let key;
+    export let expense;
+</script>
+
+<li>{expense.text}</li>
