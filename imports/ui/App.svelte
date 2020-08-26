8a2243594b7c0dceba8e894d4217d123c64ad68e
Evan Flynn <erflynn21@gmail.com>
2020-08-26 14:06:57 +0800

    fixed some bugs with expenses, refactored

--- imports/ui/App.svelte
+++ imports/ui/App.svelte
@@ -10,72 +10,100 @@
         amount: null,
         date: new Date().toISOString().substr(0, 10),
         currency: userCurrency,
-    }
+        convertedAmount: null,
+        convertCurrency: null,
+    };
 
-   
-    $: expenses = useTracker(() => Expenses.find({}, { sort: { date: -1 } }).fetch());
+    let income = {
+        title: '',
+        amount: null,
+        date: new Date().toISOString().substr(0, 10),
+        currency: userCurrency,
+    };
+
+    $: expenses = useTracker(() =>
+        Expenses.find({}, { sort: { date: -1 } }).fetch()
+    );
 
     async function handleSubmit() {
-        // insert new expense
-        if (expense.currency === userCurrency) {
-            Expenses.insert({
-                title: expense.title,
-                date: expense.date,
-                amount: expense.amount,
-                currency: expense.currency,
-            });
-            console.log(expense.amount);
+        // check whether expense needs to be converted to base currency
+        if (expense.currency !== userCurrency) {
+            await convertAmount();
         } else {
-            await convertCurrency();
-        };
-        
+            expense.currency = userCurrency;
+        }
+
+        // add the expense
+        Expenses.insert({
+            title: expense.title,
+            date: expense.date,
+            amount: expense.amount,
+            convertedAmount: expense.convertedAmount,
+            currency: expense.currency,
+            convertedCurrency: userCurrency,
+        });
 
         // clear form
         expense.title = '';
         expense.date = new Date().toISOString().substr(0, 10);
         expense.amount = '';
+        expense.currency = userCurrency;
+        expense.convertCurrency = null;
+        expense.convertedAmount = null;
     }
 
-    async function convertCurrency() {
+    async function convertAmount() {
         let url = `https://api.exchangeratesapi.io/${expense.date}?base=${userCurrency}&symbols=${expense.currency}`;
         let response = await fetch(url);
         let data = await response.json();
         let rates = JSON.stringify(data.rates);
         let exchangeRate = Number(rates.replace(/[^\d.-]/g, ''));
-        let convertedAmount = (expense.amount / exchangeRate).toFixed(2);
-        Expenses.insert({
-            title: expense.title,
-            date: expense.date,
-            amount: convertedAmount,
-            currency: expense.currency,
-        })
-
+        expense.convertedAmount = (expense.amount / exchangeRate).toFixed(2);
     }
 
-   
-  </script>
-   
-   
-  <div class="container">
+    async function handleAddIncome() {
+        console.log(income);
+    }
+</script>
+
+<div class="container">
     <header>
-      <h1>Expenses</h1>
-      <form class="new-expense" on:submit|preventDefault={handleSubmit}>
-          <input type="text" placeholder='new expense...' bind:value={expense.title}>
-          <input type="date" id='today' bind:value={expense.date}>
-          <input type="number" placeholder='amount' bind:value={expense.amount}>
-          <select id="currency" bind:value={expense.currency}>
-              <option value="USD">USD</option>
-              <option value="CNY">CNY</option>
-          </select>
-          <button on:click|preventDefault={handleSubmit}>Add</button>
-      </form>
+        <h1>Expenses</h1>
+        <form class="new-expense" on:submit|preventDefault={handleSubmit}>
+            <input
+                type="text"
+                placeholder="new expense..."
+                bind:value={expense.title} />
+            <input type="date" id="today" bind:value={expense.date} />
+            <input
+                type="number"
+                placeholder="amount"
+                bind:value={expense.amount} />
+            <select id="currency" bind:value={expense.currency}>
+                <option value="USD">USD</option>
+                <option value="CNY">CNY</option>
+            </select>
+            <button on:click|preventDefault={handleSubmit}>Add</button>
+        </form>
+        <form class="new-income" on:submit|preventDefault={handleSubmit}>
+            <input
+                type="text"
+                placeholder="new income..."
+                bind:value={income.title} />
+            <input type="date" id="today" bind:value={income.date} />
+            <input
+                type="number"
+                placeholder="amount"
+                bind:value={income.amount} />
+            <select id="currency" bind:value={income.currency}>
+                <option value="USD">USD</option>
+                <option value="CNY">CNY</option>
+            </select>
+            <button on:click|preventDefault={handleAddIncome}>Add</button>
+        </form>
     </header>
-    <ul>
-    {#each $expenses as expense}
-      <Expense
-        key={expense._id}
-        expense={expense}
-      />
+    <h1>Expenses:</h1>
+    {#each $expenses as expense (expense._id)}
+        <Expense {expense} />
     {/each}
-    </ul>
-  </div>
+</div>
--- imports/ui/Expense.svelte
+++ imports/ui/Expense.svelte
@@ -1,19 +1,26 @@
 <script>
     import { Expenses } from '../api/expenses.js';
 
-    export let key;
     export let expense;
 
     const deleteExpense = () => {
         Expenses.remove(expense._id);
-    }
+    };
 </script>
 
 <div>
-    <ul>
+    {#if expense.convertedAmount !== null}
         <button class="delete" on:click={deleteExpense}>X</button>
-        <li>{expense.title}</li>
-        <li>{expense.date}</li>
-        <li>{expense.amount}</li>
-    </ul>
+        <span>{expense.title}</span>
+        <span>{expense.date}</span>
+        <span>{expense.convertedCurrency}{expense.convertedAmount}</span>
+        <span>
+            Original Amount and Currency: {expense.currency}{expense.amount}
+        </span>
+    {:else}
+        <button class="delete" on:click={deleteExpense}>X</button>
+        <span>{expense.title}</span>
+        <span>{expense.date}</span>
+        <span>{expense.currency}{expense.amount}</span>
+    {/if}
 </div>
