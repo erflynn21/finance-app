64041b0e3ba3f3a1cee183b7298846e7957f060d
Evan Flynn <erflynn21@gmail.com>
2020-08-26 14:26:37 +0800

    incomes can now be added

--- imports/ui/App.svelte
+++ imports/ui/App.svelte
@@ -1,7 +1,9 @@
 <script>
     import { useTracker } from 'meteor/rdb:svelte-meteor-data';
     import Expense from './Expense.svelte';
+    import Income from './Income.svelte';
     import { Expenses } from '../api/expenses';
+    import { Incomes } from '../api/incomes';
 
     let userCurrency = 'CNY';
 
@@ -19,13 +21,19 @@
         amount: null,
         date: new Date().toISOString().substr(0, 10),
         currency: userCurrency,
+        convertedAmount: null,
+        convertCurrency: null,
     };
 
     $: expenses = useTracker(() =>
         Expenses.find({}, { sort: { date: -1 } }).fetch()
     );
 
-    async function handleSubmit() {
+    $: incomes = useTracker(() =>
+        Incomes.find({}, { sort: { date: -1 } }).fetch()
+    );
+
+    async function handleAddExpense() {
         // check whether expense needs to be converted to base currency
         if (expense.currency !== userCurrency) {
             await convertAmount();
@@ -52,24 +60,53 @@
         expense.convertedAmount = null;
     }
 
+    async function handleAddIncome() {
+        // check whether expense needs to be converted to base currency
+        if (income.currency !== userCurrency) {
+            await convertAmount();
+        } else {
+            income.currency = userCurrency;
+        }
+
+        // add the income
+        Incomes.insert({
+            title: income.title,
+            date: income.date,
+            amount: income.amount,
+            convertedAmount: income.convertedAmount,
+            currency: income.currency,
+            convertedCurrency: userCurrency,
+        });
+
+        // clear form
+        income.title = '';
+        income.date = new Date().toISOString().substr(0, 10);
+        income.amount = '';
+        income.currency = userCurrency;
+        income.convertCurrency = null;
+        income.convertedAmount = null;
+    }
+
     async function convertAmount() {
         let url = `https://api.exchangeratesapi.io/${expense.date}?base=${userCurrency}&symbols=${expense.currency}`;
         let response = await fetch(url);
         let data = await response.json();
         let rates = JSON.stringify(data.rates);
         let exchangeRate = Number(rates.replace(/[^\d.-]/g, ''));
-        expense.convertedAmount = (expense.amount / exchangeRate).toFixed(2);
-    }
-
-    async function handleAddIncome() {
-        console.log(income);
+        if (expense) {
+            expense.convertedAmount = (expense.amount / exchangeRate).toFixed(
+                2
+            );
+        } else {
+            income.convertedAmount = (income.amount / exchangeRate).toFixed(2);
+        }
     }
 </script>
 
 <div class="container">
     <header>
         <h1>Expenses</h1>
-        <form class="new-expense" on:submit|preventDefault={handleSubmit}>
+        <form class="new-expense" on:submit|preventDefault={handleAddExpense}>
             <input
                 type="text"
                 placeholder="new expense..."
@@ -83,9 +120,9 @@
                 <option value="USD">USD</option>
                 <option value="CNY">CNY</option>
             </select>
-            <button on:click|preventDefault={handleSubmit}>Add</button>
+            <button on:click|preventDefault={handleAddExpense}>Add</button>
         </form>
-        <form class="new-income" on:submit|preventDefault={handleSubmit}>
+        <form class="new-income" on:submit|preventDefault={handleAddIncome}>
             <input
                 type="text"
                 placeholder="new income..."
@@ -106,4 +143,9 @@
     {#each $expenses as expense (expense._id)}
         <Expense {expense} />
     {/each}
+    <h1>Incomes:</h1>
+    {#each $incomes as income (income._id)}
+        <Income {income} />
+    {/each}
+
 </div>
---
+++ imports/ui/Income.svelte
@@ -0,0 +1,26 @@
+<script>
+    import { Incomes } from '../api/incomes.js';
+
+    export let income;
+
+    const deleteIncome = () => {
+        Incomes.remove(income._id);
+    };
+</script>
+
+<div>
+    {#if income.convertedAmount !== null}
+        <button class="delete" on:click={deleteIncome}>X</button>
+        <span>{income.title}</span>
+        <span>{income.date}</span>
+        <span>{income.convertedCurrency}{income.convertedAmount}</span>
+        <span>
+            Original Amount and Currency: {income.currency}{income.amount}
+        </span>
+    {:else}
+        <button class="delete" on:click={deleteIncome}>X</button>
+        <span>{income.title}</span>
+        <span>{income.date}</span>
+        <span>{income.currency}{income.amount}</span>
+    {/if}
+</div>
