91ac5eb2a4415a2b6958b00371f39bf46f7b531e
Evan Flynn <erflynn21@gmail.com>
2020-08-27 13:46:24 +0800

    got total expense sum somewhat working

--- imports/ui/components/ExpenseList.svelte
+++ imports/ui/components/ExpenseList.svelte
@@ -6,13 +6,30 @@
     $: expenses = useTracker(() =>
         Expenses.find({}, { sort: { date: -1 } }).fetch()
     );
+
+    $: expenseSum = totalExpenses.reduce(function (a, b) {
+        return a + b;
+    }, 0);
+
+    let totalExpenses = [];
+    let totalExpenseIDs = [];
+    const calculateTotal = (expense) => {
+        if (totalExpenseIDs.includes(expense._id) === false) {
+            totalExpenseIDs = [...totalExpenseIDs, expense._id];
+            totalExpenses = [...totalExpenses, expense.amount];
+            console.log(totalExpenses);
+        }
+    };
 </script>
 
 <div>
     <h1>Expenses:</h1>
     <!-- List of expenses -->
+    {#each $expenses.map(calculateTotal) as expense}
+        <div />
+    {/each}
     {#each $expenses as expense (expense._id)}
         <Expense {expense} />
     {/each}
-    <h3>Total Expenses:</h3>
+    <h3>Total Expenses: {expenseSum}</h3>
 </div>
