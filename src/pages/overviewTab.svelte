a54e2183cfa0a12819a3c7c99d05cb2d98968583
Evan Flynn <erflynn21@gmail.com>
2020-11-16 11:20:32 +0800

    styles tranactions page

--- src/pages/overviewTab.svelte
+++ src/pages/overviewTab.svelte
@@ -1,42 +1,7 @@
 <script>
     import { Page, Navbar } from 'framework7-svelte';
-    import {
-        deleteExpense,
-        expenses,
-        expensesSum,
-    } from '../stores/expensesStore';
-    import { deleteIncome, incomes, incomesSum } from '../stores/incomesStore';
 </script>
 
 <Page name="home">
     <Navbar title="Overview" />
-    expenses:
-    {#each $expenses as { item, itemId } (itemId)}
-        {item.currency}
-        {item.title}
-        {item.amount}
-        {item.category}
-        {#if item.originalAmount}
-            {item.originalCurrency}
-            {item.originalAmount}
-        {/if}
-        <button on:click={() => deleteExpense(itemId)}>X</button>
-        <br />
-    {/each}
-    {$expensesSum}
-
-    <br />
-    incomes:
-    {#each $incomes as { item, itemId } (itemId)}
-        {item.currency}
-        {item.title}
-        {item.amount}
-        {#if item.originalAmount}
-            {item.originalCurrency}
-            {item.originalAmount}
-        {/if}
-        <button on:click={() => deleteIncome(itemId)}>X</button>
-        <br />
-    {/each}
-    {$incomesSum}
 </Page>
--- src/pages/transactionsTab.svelte
+++ src/pages/transactionsTab.svelte
@@ -1,7 +1,51 @@
 <script>
     import { Page, Navbar } from 'framework7-svelte';
+    import BlockHeader from 'framework7-svelte/components/block-header.svelte';
+    import BlockTitle from 'framework7-svelte/components/block-title.svelte';
+    import List from 'framework7-svelte/components/list.svelte';
+    import SwiperItem from '../components/swiperItem.svelte';
+    import {
+        deleteExpense,
+        expenses,
+        expensesSum,
+    } from '../stores/expensesStore';
+    import { deleteIncome, incomes, incomesSum } from '../stores/incomesStore';
 </script>
 
 <Page name="transactions">
     <Navbar title="Transactions" />
+    <h1>Expenses:</h1>
+    <h3>Total Expenses: {$expensesSum}</h3>
+
+    <List>
+        {#each $expenses as { item, itemId } (itemId)}
+            <SwiperItem {item} on:deleted={() => deleteExpense(itemId)} />
+        {/each}
+    </List>
+    <h1>Income:</h1>
+    <h3>Total Income: {$incomesSum}</h3>
+
+    <List>
+        {#each $incomes as { item, itemId } (itemId)}
+            <SwiperItem {item} on:deleted={() => deleteIncome(itemId)} />
+        {/each}
+    </List>
 </Page>
+
+<style>
+    h1 {
+        font-size: 16px;
+        color: black;
+        font-weight: 400;
+        text-align: center;
+        margin: 10px 0;
+    }
+
+    h3 {
+        font-size: 14px;
+        color: black;
+        font-weight: 400;
+        text-align: center;
+        margin: 10px 0;
+    }
+</style>
