00564771ae18e1a9d82bb9f092bf90480bbe81d2
Evan Flynn <erflynn21@gmail.com>
2020-11-16 10:26:54 +0800

    adds swiper list component

---
+++ src/components/swiperList.svelte
@@ -0,0 +1,36 @@
+<script>
+    export let item;
+    import ListItem from 'framework7-svelte/components/list-item.svelte';
+    import List from 'framework7-svelte/components/list.svelte';
+    import SwipeoutActions from 'framework7-svelte/components/swipeout-actions.svelte';
+    import SwipeoutButton from 'framework7-svelte/components/swipeout-button.svelte';
+    import { createEventDispatcher } from 'svelte';
+    const dispatch = createEventDispatcher();
+</script>
+
+<List>
+    <ListItem
+        swipeout
+        title={item.title}
+        after={item.amount}
+        subtitle="{item.currency}{item.category}"
+        onSwipeoutDeleted={() => dispatch('deleted')}>
+        <SwipeoutActions left>
+            <SwipeoutButton
+                overswipe
+                color="green"
+                onClick={() => console.log('edit')}>
+                Edit
+            </SwipeoutButton>
+        </SwipeoutActions>
+        <SwipeoutActions right>
+            <SwipeoutButton
+                delete
+                overswipe
+                confirmTitle=" "
+                confirmText="Are you sure you want to delete this item?">
+                Delete
+            </SwipeoutButton>
+        </SwipeoutActions>
+    </ListItem>
+</List>
--- src/pages/monthlyExpenses.svelte
+++ src/pages/monthlyExpenses.svelte
@@ -1,9 +1,10 @@
 <script>
     import { Page, Navbar } from 'framework7-svelte';
-    import { get } from 'svelte/store';
+    import SwiperList from '../components/swiperList.svelte';
     import {
         deleteMonthlyExpense,
         monthlyExpenses,
+        updateMonthlyExpense,
     } from '../stores/monthlyExpensesStore';
 </script>
 
@@ -11,12 +12,6 @@
     <!-- Top Navbar -->
     <Navbar title="Monthly Recurring Expenses" backLink="Back" />
     {#each $monthlyExpenses as { item, itemId } (itemId)}
-        {item.currency}
-        {item.title}
-        {item.amount}
-        {item.category}
-        <button on:click={() => deleteMonthlyExpense(itemId)}>X</button>
-        <br />
+        <SwiperList {item} on:deleted={() => deleteMonthlyExpense(itemId)} />
     {/each}
-    hi
 </Page>
