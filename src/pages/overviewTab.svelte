8f7e302d7a272b65b878a254a537debac895b38f
Evan Flynn <erflynn21@gmail.com>
2020-11-18 11:17:57 +0800

    fixes issue with ios simulator

--- src/pages/overviewTab.svelte
+++ src/pages/overviewTab.svelte
@@ -2,11 +2,9 @@
     import { Page, Navbar } from 'framework7-svelte';
     import List from 'framework7-svelte/components/list.svelte';
     import ListItem from 'framework7-svelte/components/list-item.svelte';
-
-    console.log('overview page');
 </script>
 
 <Page name="home">
     <Navbar title="Overview" />
-    hey there from the overview spage
+    hey there from the overview tab
 </Page>
